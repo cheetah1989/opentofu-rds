08d21884cec3a6c3a5e15e7e774b1471b7695324
chetan2pune <chetan2pune@yahoo.com>
2026-01-19 03:25:24 +0530

    Rectified rds db subnet to comply with AZ

--- opentofu_rds_iac/vpc_subnet.tf
+++ opentofu_rds_iac/vpc_subnet.tf
@@ -16,6 +16,7 @@ resource "aws_subnet" "opentofuRDS_pubsubnet" {
 resource "aws_subnet" "opentofuRDS_privsubnet1" {
   vpc_id     = aws_vpc.opentofuRDS_vpc.id
   cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-south-1a"
   tags = {
     Purpose = "opentofuRDS_private_subnet"
   }
@@ -24,6 +25,7 @@ resource "aws_subnet" "opentofuRDS_privsubnet1" {
 resource "aws_subnet" "opentofuRDS_privsubnet2" {
   vpc_id     = aws_vpc.opentofuRDS_vpc.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-south-1b"
   tags = {
     Purpose = "opentofuRDS_private_subnet"
   }
